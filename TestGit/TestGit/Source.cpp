5db2c698fa93e022d93b6201511b707417871049
Grovar <stphngbbns2@gmail.com>
2015-09-02 15:55:51 -0500

    Nothing but a G thang.

--- TestGit/TestGit/Source.cpp
+++ TestGit/TestGit/Source.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	cout << "this is just a git test." << endl;
+	cout << "this is just a git test. Nothing but a Git thang." << endl;
 	cout << "push more" << endl;
 	char a;
 	cin >> a;
